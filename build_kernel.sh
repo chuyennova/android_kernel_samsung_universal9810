cd9a43bcae90ebef020ce081708deae5eee477eb
xxmustafacooTR <mustafa.gokmen2004@gmail.com>
2022-05-19 22:00:25 +0300

    Change Toolchain and Disable Graphite

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-export CROSS_COMPILE=/home/$USER/Android/Toolchains/aarch64-elf/bin/aarch64-elf-
+export CROSS_COMPILE=/home/$USER/Android/Toolchains/aarch64-linux-gnu/bin/aarch64-linux-gnu-
 export CROSS_COMPILE_ARM32=/home/$USER/Android/Toolchains/arm-eabi/bin/arm-eabi-
+export CLANG_TRIPLE=/home/$USER/Android/Toolchains/aarch64-elf/bin/aarch64-elf-
 export CC=/home/$USER/Android/Toolchains/clang/bin/clang
 export LDLLD=/home/$USER/Android/Toolchains/clang/bin/ld.lld
 
